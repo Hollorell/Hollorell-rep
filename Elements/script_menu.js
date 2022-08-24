23eb7f15d4df33ab3211ded11b8ad6873e77f4ca
Hollorell <111856439+Hollorell@users.noreply.github.com>
2022-08-24 18:06:18 +0300

    Add files via upload

---
+++ Elements/script_menu.js
@@ -0,0 +1,8 @@
+function show_hide() {
+    let click = document.getElementById("myDropdown");
+    if (click.style.display === "none") {
+        click.style.display = "block";
+    } else {
+        click.style.display = "none";
+    }
+}  
